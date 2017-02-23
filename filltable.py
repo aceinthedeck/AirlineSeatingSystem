9c17f65f6234c1d2d9b32ab9f18c19acdfedb7d6
aceinthedeck <ace120387@gmail.com>
2017-02-23 15:51:16 +0000

    Testing Code

--- filltable.py
+++ filltable.py
@@ -1,12 +1,25 @@
 import sqlite3
 import csv
-
+import random
 
 
 
 # This program fills the database for testing 							#
 # fillRowsColsTable function fills the seating arrangement				#
 # fillTable fills the seating table according to the seating arrangment #
+
+
+def clearSeatingTable(fileName):
+	conn=sqlite3.connect(fileName)
+	c=conn.cursor()
+	cmd='Delete from seating'
+	try:
+		c.execute(cmd)
+		conn.commit()
+		conn.close()
+	except sqlite3.Error as er:
+		print(er.message)
+		conn.close()
 
 def fillTable(fileName,row,seat):
 	conn=sqlite3.connect(fileName)
@@ -50,21 +63,39 @@ def createBookingsCSV(fileName,nameList):
 
 	with open(fileName,'w',newline='') as csvFile:
 		fileWriter=csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		fileWriter.writerow(['Ace',1])
+		for name in nameList:
+			fileWriter.writerow([name[0],random.randint(1,5)])
 
 
-createBookingsCSV('test.csv',['Ace'])
-print("file created")
+def getNames(fileName):
+	names=[]
+	try:
+		with open(fileName) as f:
+			reader=csv.reader(f)
+			for row in reader:
+				names.append(row)
+			return names
+	except:
+		print("Unable to open the file containing random names")
 
+randomNames=[]
+namesCSV='names.csv'
 fileName='data.db'
 seats='ABCDE'
 totalRows=20
+cols=0
 
-#fillRowsColsTable(fileName,totalRows,seats)
+randomNames=getNames(namesCSV)
+createBookingsCSV('test.csv',randomNames)
 
-#for i in range(1,totalRows+1):
-#	for seat in seats:
-#		fillTable(fileName,i,seat)
+print("Testfile created")
 
+clearSeatingTable(fileName)
 
-#print("Database updated")
+fillRowsColsTable(fileName,totalRows,seats)
+
+for i in range(1,totalRows+1):
+	for seat in seats:
+		fillTable(fileName,i,seat)
+
+print("Database updated")
